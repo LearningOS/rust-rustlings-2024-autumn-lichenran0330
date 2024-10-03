98e1aa1d37c6de69d1ba1ac99c87a57308584727
lichenran <lichenran0330@gmail.com>
2024-10-03 18:52:01 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,31 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut string = input.to_string();
+    let x = " ";
+    while string[0..1] == x[0..1] {
+        string.remove(0);
+    }
+    while string[string.len() - 1..string.len()] == x[0..1] {
+        string.pop();
+    }
+    string
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut string = input.to_string();
+    string.push_str(" world!");
+    string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut string = input.to_string();
+    string.replace("cars", "balloons")
 }
 
 #[cfg(test)]
