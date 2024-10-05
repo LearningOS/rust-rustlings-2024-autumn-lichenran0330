5d05c9fbc9c6b073ee924e188a1304a375ffe8c3
lichenran <lichenran0330@gmail.com>
2024-10-05 19:06:42 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,8 +18,23 @@
 
 // I AM NOT DONE
 
+pub trait Pt{
+    fn pt(&self) -> String;
+}
+
+impl Pt for f32 {
+    fn pt(&self) -> String {
+        self.to_string()
+    }
+}
+impl Pt for String {
+    fn pt(&self) -> String {
+        self.to_string()
+    }
+}
+
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: &dyn Pt,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -27,7 +42,7 @@ pub struct ReportCard {
 impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.pt())
     }
 }
 
@@ -52,7 +67,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
