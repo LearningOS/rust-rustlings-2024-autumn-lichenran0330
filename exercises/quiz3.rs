13c1dcb178d5975a4aaf465a78f0b3fe75f97137
lichenran <lichenran0330@gmail.com>
2024-10-06 21:30:18 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub trait Pt{
     fn pt(&self) -> String;
@@ -33,13 +32,13 @@ impl Pt for String {
     }
 }
 
-pub struct ReportCard {
-    pub grade: &dyn Pt,
+pub struct ReportCard<'a> {
+    pub grade: &'a dyn Pt,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl ReportCard<'_> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade.pt())
@@ -53,7 +52,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: &2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -67,7 +66,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: "A+",
+            grade: &"A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
